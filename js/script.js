8b7f4020f0b1bc0c3caa30e0984bca325f8a1976
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-08-06 01:55:21 -0500

    Escuchar mediante onclick y onchange}

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const claseParrafito = document.querySelector('.parrafito');
-const idParrafito = document.querySelector('#parrafito');
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
 
-console.log({
-    h1, 
-    p,
-    claseParrafito,
-    idParrafito,
-    input,
-});
-
-
-
-h1.innerText="Modificando Texto del HTML desde JS";
-h1?.classList.add("red");
-h1?.classList.toggle("green");
-
- 
- 
- h1?.classList.add("red");
-h1?.classList.toggle("green");
-
-const img = document.createElement("img");
-img.setAttribute('src','https://static.platzi.com/media/achievements/practico-javascript_badge-fb533fa5-73c8-4f75-9d81-0294d9013ab5.png')
-
-idParrafito?.append(img);
