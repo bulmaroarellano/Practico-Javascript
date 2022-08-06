511acbcd372ab54d0eb3a9db0a909e12b5571222
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-08-06 14:28:10 -0500

    event.preventDefault para submit orms

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const pResult = document.querySelector("#result");
+const btn=document.querySelector("#btnCalcular");
+const form = document.querySelector("form");
 
-function btnOnClick(){ 
+
+function sumarInputValues(event){
+    event.preventDefault(); 
     const sumaInputs = input1.value + input2.value;
     pResult.innerText="Resultado: "+parseInt(sumaInputs);
-
-
 }
+
+form.addEventListener('submit', sumarInputValues);
+
