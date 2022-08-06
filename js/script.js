6265f6bf8f65db10d8d729aa8345d238f24665c2
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-08-06 00:27:21 -0500

    Conectar html y js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const title = document.querySelector("h1");
+const p = document.querySelector("p");
+const claseParrafito = document.querySelector('.parrafito');
+const idParrafito = document.querySelector('#parrafito');
+const input = document.querySelector("input");
+
+console.log({
+    title, 
+    p,
+    claseParrafito,
+    idParrafito,
+    input,
+});
