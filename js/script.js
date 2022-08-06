fdf9cc581f4ef9348f3e4c8e5e1ad80c8cb514ed
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-08-06 13:52:29 -0500

    Escuchar mediante onclick y onchange

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
+const pResult = document.querySelector("#result");
 
+function btnOnClick(){ 
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText="Resultado: "+parseInt(sumaInputs);
+
+
+}
